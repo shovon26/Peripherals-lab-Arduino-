2f6bb13f031e2a2f1313c390e804ca9d30a5067e
shovon26 <36441192+shovon26@users.noreply.github.com>
2020-01-17 02:16:47 +0600

    Update Print HelloWorld.ino

--- Last lab 16X2 LCD Display/Print HelloWorld.ino
+++ Last lab 16X2 LCD Display/Print HelloWorld.ino
@@ -1,6 +1,13 @@
 #include<LiquidCrystal.h>
 
-LiquidCrystal lcd(12,11,5,4,3,2);
+const int rs=12,en=11,d4=5,d5=4,d6=3,d7=2;
+/*
+ * rs=register select(0/1)
+ * rs=0 --> write data into memory 
+ * rs=1 --> write control signal
+ */
+
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
